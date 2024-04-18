d1e6111e9953988deca16f70be835206846ae8a0
benaichalae <benaichalae@gmail.com>
2024-04-18 16:30:44 -0700

    py

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 """
-This module defines a function 'add' that takes two floats and returns their sum.
+This module defines a function 'add' that
+takes two floats and returns their sum.
 """
 
 
 def add(a: float, b: float) -> float:
-  """Adds two floats and returns their sum.
+    '''Adds two floats and returns their sum.
 
-  Args:
-      a: The first float number.
-      b: The second float number.
+    Args:
+        a: The first float number.
+        b: The second float number.
 
-  Returns:
-      The sum of a and b.
-  """
-  return a + b
+    Returns:
+        The sum of a and b.
+    '''
+    return (a + b)
