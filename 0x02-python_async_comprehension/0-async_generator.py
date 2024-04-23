cc242a5fd4f88752b1e1143aa00d3fda8af737bd
benaichalae <benaichalae@gmail.com>
2024-04-23 04:11:10 -0700

    py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""coroutine called async_generator that takes no arguments"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+  """
+  An asynchronous generator that yields random numbers between 0 and 10
+  after waiting for 1 second each time.
+  """
+  for _ in range(10):
+    await asyncio.sleep(1)
+    yield random.randint(0, 10)
